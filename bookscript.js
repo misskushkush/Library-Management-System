4f16e15d3378eb15dc034763079714d5eafc5a85
Liza Kushneryk <88245223+misskushkush@users.noreply.github.com>
2023-05-15 13:02:47 +0200

    Update bookscript.js

--- bookscript.js
+++ bookscript.js
@@ -1,13 +1,21 @@
 let books = [];
 
+if (localStorage.getItem("books")) {
+    books = JSON.parse(localStorage.getItem("books"));
+    for (let i = 0; i < books.length; i++) {
+      addBook(books[i]);
+    }
+  }
+
 function addBook(book){
     let table = $("#bookTable tbody");
     table.append(`
     <tr id="${book.id}">
     <td>${book.title}</td>
     <td>${book.author}</td>
-    <td>${book.genre}</td>
+    <td>${book.name}</td>
     <td>${book.year}</td>
+    <td>${book.page}</td>
     <td>${book.quantity}</td>
     <td>
         <button class="mb-1 btn btn-sm btn-warning editBtn" data-id="${book.id}">
@@ -18,13 +26,16 @@ function addBook(book){
         </button>
     </td>
     `);
+    // Save updated books to local storage
+  localStorage.setItem("books", JSON.stringify(books));
 }
 
 function clearForm(){
     $("#bookTitle").val("");
     $("#author").val("");
-    $("#genre").val("");
-    $("#year").val("");
+    $("#publishName").val("");
+    $("#publishYear").val("");
+    $("#pageQuantity").val("");
     $("#quantity").val("");
 }
 
@@ -43,8 +54,9 @@ $("#bookForm").submit(function (e){
         id: generateId(),
         title: $("#bookTitle").val(),
         author: $("#author").val(),
-        genre: $("#genre").val(),
-        year: $("#year").val(),
+        name: $("#publishName").val(),
+        year: $("#publishYear").val(),
+        page: $("#pageQuantity").val(),
         quantity: $("#quantity").val(),
     };
 
@@ -52,4 +64,69 @@ $("#bookForm").submit(function (e){
     addBook(book);
 
     clearForm();
+    localStorage.setItem('books', JSON.stringify(books));
+});
+
+$("#editForm").submit(function (e){
+    e.preventDefault();
+
+    let bookId = $("#editBookId").val();
+    let bookIndex = books.findIndex((book)=>book.id == bookId);
+    let book = books[bookIndex];
+    book.title = $("#editbookTitle").val();
+    book.author = $("#editauthor").val();
+    book.name = $("#editpublishName").val();
+    book.year = $("#editpublishYear").val();
+    book.page = $("#editpageQuantity").val();
+    book.quantity = $("#editquantity").val();
+
+
+    let row = $(`#${book.id}`);
+    row.find("td:eq(0)").text(book.title);
+    row.find("td:eq(1)").text(book.author);
+    row.find("td:eq(2)").text(book.name);
+    row.find("td:eq(3)").text(book.year);
+    row.find("td:eq(4)").text(book.page);
+    row.find("td:eq(5)").text(book.quantity);
+
+    $("#editModal").modal("hide");
+    // Save updated books to local storage
+  localStorage.setItem("books", JSON.stringify(books));
+
+});
+
+$(document).on("click", ".editBtn", function () {
+    let bookId = $(this).data("id");
+
+    let bookIndex = books.findIndex((book) => book.id == bookId);
+    let book = books[bookIndex];
+
+    $("#editbookTitle").val(book.title);
+    $("#editauthor").val(book.author);
+    $("#editpublishName").val(book.name);
+    $("#editpublishYear").val(book.year);
+    $("#editpageQuantity").val(book.page);
+    $("#editquantity").val(book.quantity);
+    $("#editBookId").val(book.id);
+
+    $("#editModal").modal("show");
+    localStorage.setItem('books', JSON.stringify(books));
+});
+
+$(document).on("click", "#clsBtn", function () {
+    $("#editModal").modal("hide");
+});
+
+$(document).on("click", ".deleteBtn", function () {
+    let bookId = $(this).data("id");
+
+    let bookIndex = books.findIndex((book) => book.id == bookId);
+    let book = books[bookIndex];
+
+    if (confirm(`Are you sure you want to delete 
+    ${book.title}`)) {
+        books.splice(bookIndex, 1);
+        $(`#${book.id}`).remove();
+    }
+    localStorage.setItem('books', JSON.stringify(books));
 });
